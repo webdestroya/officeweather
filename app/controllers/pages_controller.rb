d45dec84772e68a038d9b17c3ecbabfd1958fbec
Mitch Dempsey <mrdempsey@gmail.com>
2012-07-27 11:28:57 -0700

    pages update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,13 +9,13 @@ class PagesController < ApplicationController
     end
 
     #TempReading.average(:temperature, :group => " strftime('%H',created_at)")
-    hourlyavgs = TempReading.average(:temperature, :group => "strftime('%H',created_at)")
+    hourlyavgs = TempReading.average(:temperature, :group => "EXTRACT(HOUR from created_at)")
     @hourly = []
     hourlyavgs.each_pair do |key,val|
       @hourly << [key.to_i, val.to_f]
     end
 
-    dowavgs = TempReading.average(:temperature, :group => "strftime('%w',created_at)")
+    dowavgs = TempReading.average(:temperature, :group => "EXTRACT(dow from created_at)")
     @dow = []
     dowavgs.each_pair do |key,val|
       @dow << [key.to_i, val.to_f]
