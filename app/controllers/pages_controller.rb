17762d9399f7b7d8df149ba005eddff8ace35b67
Mitch Dempsey <mrdempsey@gmail.com>
2012-11-05 12:48:39 -0800

    removing other graphics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,11 +13,23 @@ class PagesController < ApplicationController
       @temps << [(temp.ts.to_i*1000), temp.value]
     end
 
-    templist2 = TempReading.where(["created_at >= ?", 1.week.ago]).all
+    # templist2 = TempReading.where(["created_at >= ?", 1.week.ago]).all
+    # @temp_week = []
+    # templist2.each do |temp|
+    #   @temp_week << [(temp.created_at.to_i*1000), temp.temperature]
+    # end
+
+
     @temp_week = []
-    templist2.each do |temp|
-      @temp_week << [(temp.created_at.to_i*1000), temp.temperature]
+    templist = tempodb.read_key("office_temp", 1.week.ago.utc, Time.now.utc, :interval => "15min", :function => "avg")
+    templist.data.each do |temp|
+      @temp_week << [(temp.ts.to_i*1000), temp.value]
     end
+
+    @hourly = nil
+    @hourly_min = nil
+    @hourly_max = nil
+    @dow = nil
 
     # Hourly
     # @hourly = chart_grouper TempReading.average(:temperature, :group => "EXTRACT(HOUR from created_at)")    
@@ -28,10 +40,14 @@ class PagesController < ApplicationController
     # @dow = chart_grouper TempReading.average(:temperature, :group => "EXTRACT(DOW from created_at)")
 
 
-
+    @high = tempodb.read_key("office_temp", 1.year.ago.utc, Time.now.utc, :interval => "1year", :function => "max").data[0]
+    @low = tempodb.read_key("office_temp", 1.year.ago.utc, Time.now.utc, :interval => "1year", :function => "min").data[0]
+       
     # @high = TempReading.order("temperature DESC").first
     # @low = TempReading.order("temperature ASC").first
 
+    @current = tempodb.read_key("office_temp", 10.minutes.ago, Time.now, :interval => "raw").data.last
+    
   end
 
 
